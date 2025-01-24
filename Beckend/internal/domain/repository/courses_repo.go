6bd34e8cfbeb425cc2e55b028a6ababd6857d755
Naufela Rashif <naufelarashif@gmail.com>
2025-01-24 23:23:17 +0700

    change clean architecture to fiber Clean Architecture

--- Beckend/internal/domain/repository/courses_repo.go
+++ Beckend/internal/domain/repository/courses_repo.go
@@ -2,8 +2,11 @@ package repository
 
 import (
 	"Udummy/internal/domain/entity"
+	repository2 "Udummy/internal/infrastructure/repository"
 	"context"
 	"database/sql"
+	_ "database/sql/driver"
+	"errors"
 )
 
 type CoursesRepository interface {
@@ -12,4 +15,61 @@ type CoursesRepository interface {
 	Delete(ctx context.Context, tx *sql.Tx, course entity.Courses)
 	FindAll(ctx context.Context, tx *sql.Tx) []entity.Courses
 	FindById(ctx context.Context, tx *sql.Tx, courseId int) (entity.Courses, error)
+}
+type CoursesRepoImpl struct {
+}
+
+func (repo *CoursesRepoImpl) Create(ctx context.Context, tx *sql.Tx, course entity.Courses) entity.Courses {
+	SQL := `INSERT INTO courses (id, name,) VALUES (?, ?, ?)`
+	result, err := tx.ExecContext(ctx, SQL, course.Name)
+	repository2.PanicIfError(err)
+
+	id, err := result.LastInsertId()
+	repository2.PanicIfError(err)
+
+	course.Id = int(id)
+	return course
+}
+
+func (repo *CoursesRepoImpl) Update(ctx context.Context, tx *sql.Tx, course entity.Courses) entity.Courses {
+	SQL := `UPDATE courses SET name = ? WHERE id = ?`
+	_, err := tx.ExecContext(ctx, SQL, course.Name, course.Id)
+	repository2.PanicIfError(err)
+	return course
+
+}
+
+func (repo *CoursesRepoImpl) Delete(ctx context.Context, tx *sql.Tx, course entity.Courses) {
+	SQL := `DELETE FROM courses WHERE id = ?`
+	_, err := tx.ExecContext(ctx, SQL, course.Id)
+	repository2.PanicIfError(err)
+}
+
+func (repo *CoursesRepoImpl) FindAll(ctx context.Context, tx *sql.Tx) []entity.Courses {
+	SQL := `SELECT id, name FROM courses`
+	rows, err := tx.QueryContext(ctx, SQL)
+	repository2.PanicIfError(err)
+
+	var courses []entity.Courses
+	for rows.Next() {
+		course := entity.Courses{}
+		err := rows.Scan(&course.Id, &course.Name)
+		repository2.PanicIfError(err)
+		courses = append(courses, course)
+	}
+	return courses
+}
+
+func (repo *CoursesRepoImpl) FindById(ctx context.Context, tx *sql.Tx, courseId int) (entity.Courses, error) {
+	SQL := `SELECT id, name FROM courses WHERE id = ?`
+	rows, err := tx.QueryContext(ctx, SQL, courseId)
+	repository2.PanicIfError(err)
+	courses := entity.Courses{}
+	if rows.Next() {
+		err := rows.Scan(&courses.Id, &courses.Name)
+		repository2.PanicIfError(err)
+		return entity.Courses{}, err
+	} else {
+		return entity.Courses{}, errors.New("courses not found")
+	}
 }
--- Beckend/internal/domain/usecase/courses/courses_usecase.go
+++ Beckend/internal/domain/usecase/courses/courses_usecase.go
@@ -3,7 +3,7 @@ package usecase
 import "context"
 
 type CoursesUsecase interface {
-	Create(ctx context.Context)
+	Create(ctx context.Context, request CoursesUsecaseRequest) CourseResponse
 	Update(ctx context.Context)
 	Delete(ctx context.Context)
 	FindAll(ctx context.Context)
